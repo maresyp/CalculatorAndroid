7c4bbdb9ceb2c968718401522648e46c94d4873e
maresyp <mattq.2000@icloud.com>
2023-12-04 11:21:10 +0100

    add handler for button with numbers

--- app/src/main/java/com/example/calculator/activities/SimpleCalculator.java
+++ app/src/main/java/com/example/calculator/activities/SimpleCalculator.java
@@ -2,35 +2,57 @@ package com.example.calculator.activities;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
-
-import com.example.calculator.Calculator;
-import com.example.calculator.viewModels.CalcViewModel;
-import com.example.calculator.enums.Context;
 import com.example.calculator.R;
 
+import java.util.Locale;
+
 public class SimpleCalculator extends AppCompatActivity {
-    private CalcViewModel viewModel;
+    private TextView resultTextView;
     private Double CalculaitonResult;
+    private final int MAX_LENGTH = 9;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        viewModel = new ViewModelProvider((Calculator) getApplication()).get(CalcViewModel.class);
-        viewModel.getContext().setValue(Context.SIMPLE_CALC);
-
-        System.out.println("ELO ELO");
+        setContentView(R.layout.activity_simple_calculator);
 
         CalculaitonResult = 0.0D;
 
-        setContentView(R.layout.activity_simple_calculator);
+        resultTextView = findViewById(R.id.resultTextView);
 
     }
 
-    public void fiveOnClick(View view) {
+    public void numberButtonHandler(View view) {
+        String clicked_number = ((TextView) view).getText().toString();
+        String current_on_display = resultTextView.getText().toString();
 
+        try {
+            if (current_on_display.contains(",")) {
+                if (current_on_display.length() >= this.MAX_LENGTH + 1) {
+                    throw new Exception();
+                }
+            } else if (current_on_display.length() == this.MAX_LENGTH) {
+                throw new Exception();
+            }
+        } catch (Exception e ) {
+            // TODO : Add error message
+            return;
+        }
+
+
+        if (current_on_display.equals("0") || current_on_display.equals("-0")) {
+            if (clicked_number.equals("0")) {
+                return;
+            }
+            resultTextView.setText(clicked_number);
+        } else {
+            resultTextView.setText(String.format(Locale.getDefault(), "%s", current_on_display + clicked_number));
+        }
+    }
+
+    public void decimalOnClick(View view) {
     }
 }
