b22c13e98b8c77bf5127c77057baed6de586f1de
maresyp <mattq.2000@icloud.com>
2023-12-06 10:11:08 +0100

    Refactor Calculator class

--- app/src/main/java/com/example/calculator/Calculator.java
+++ app/src/main/java/com/example/calculator/Calculator.java
@@ -2,14 +2,6 @@ package com.example.calculator;
 
 import android.app.Application;
 
-import androidx.lifecycle.ViewModelStore;
-import androidx.lifecycle.ViewModelStoreOwner;
+public class Calculator extends Application {
 
-public class Calculator extends Application implements ViewModelStoreOwner {
-    private final ViewModelStore viewModelStore = new ViewModelStore();
-
-    @Override
-    public ViewModelStore getViewModelStore() {
-        return viewModelStore;
-    }
 }
